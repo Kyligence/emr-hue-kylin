f5dc51c909cc51610e39f8dd5789e86592a0a013
YongjieZhao <yongjie.zhao@gmail.com>
2017-12-05 17:55:17 +0800

    escape json string

--- download.sh
+++ download.sh
@@ -2,7 +2,3 @@
 set -e
 sudo mkdir -p /opt/kylin_jdbc
 sudo wget -S -T 10 -t 5 https://raw.githubusercontent.com/Kyligence/emr-hue-kylin/master/kylin-jdbc-2.2.0.jar -O /opt/kylin_jdbc/kylin-jdbc-2.2.0.jar
-
-sudo sed -i "s/'/\"/g" /etc/hue/conf/hue.ini
-sudo sed -i "s/\"{/'{/g" /etc/hue/conf/hue.ini
-sudo sed -i "s/}\"/}'/g" /etc/hue/conf/hue.ini
