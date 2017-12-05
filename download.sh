61155425bc461e88d4e2c844bb4110d3b50a74d2
YongjieZhao <yongjie.zhao@gmail.com>
2017-12-05 17:12:29 +0800

    updates download

--- download.sh
+++ download.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e
-mkdir -p /opt/kylin_jdbc
-wget -S -T 10 -t 5 https://raw.githubusercontent.com/Kyligence/emr-hue-kylin/master/kylin-jdbc-2.2.0.jar -O /opt/kylin_jdbc
+sudo mkdir -p /opt/kylin_jdbc
+sudo wget -S -T 10 -t 5 https://raw.githubusercontent.com/Kyligence/emr-hue-kylin/master/kylin-jdbc-2.2.0.jar -O /opt/kylin_jdbc/kylin-jdbc-2.2.0.jar
+
+sudo sed -i "s/'/\"/g" /etc/hue/conf/hue.ini
+sudo sed -i "s/\"{/'{/g" /etc/hue/conf/hue.ini
+sudo sed -i "s/}\"/}'/g" /etc/hue/conf/hue.ini
